9ac2f6685ddda0890d0f6a96d28a3f0d61e36248
Lundev42 <vetleoyvind@yahoo.com>
2026-02-10 15:36:19 +0100

    implementerte read_file

--- wordfrequency.py
+++ wordfrequency.py
@@ -17,7 +17,10 @@ def read_file(file_name):
     tilbake en liste av tekststrenger som representerer linjene i filen.
     """
     # Tips: kanksje "open"-funksjonen kunne være nyttig her: https://docs.python.org/3/library/functions.html#open
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    with open(file_name, 'r') as f:
+        lines = f.read().splitlines()
+        #lines = f.readlines()
+    return lines  # Return the list of lines
 
 
 def lines_to_words(lines):
